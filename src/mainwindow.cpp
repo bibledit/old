d01ea7a83a028bac0bb49cf12b3814a56756ffa4
teus <teus@nw9440.(none)>
2008-05-14 18:24:05 +0200

    fix make install on Mac

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7397,3 +7397,19 @@ void MainWindow::on_preferences_filters ()
   FiltersDialog dialog (0);
   dialog.run ();
 }
+
+
+// Todo bypass problem with eeepc on Macintosh: let is always exit 0.
+
+
+/*
+Todo note navigation upgrade
+
+. When the cursor is in a footnote, let UpArrow / DownArrow go the the
+previous or next note.
+
+2. When clicking on the note caller in the text, let the cursor go to the
+note itself, to be edited.
+
+
+*/
