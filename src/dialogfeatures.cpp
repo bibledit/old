f6bc9151e9c4d353c244a4c27da9c5fe3dbd36fe
Teus Benschop <teus@2510p.(none)>
2008-12-17 18:50:21 +0200

    documentation of features was updated

--- src/dialogfeatures.cpp
+++ src/dialogfeatures.cpp
@@ -138,7 +138,7 @@ FeaturesDialog::FeaturesDialog(int dummy) {
   gtk_widget_show(dialog_action_area1);
   gtk_button_box_set_layout(GTK_BUTTON_BOX (dialog_action_area1), GTK_BUTTONBOX_END);
 
-  new InDialogHelp (featuresdialog, &shortcuts, NULL);
+  new InDialogHelp (featuresdialog, &shortcuts, "preferences_features");
 
   cancelbutton = gtk_button_new_from_stock("gtk-cancel");
   gtk_widget_show(cancelbutton);
