1bfd603921aa1a6de0eb7295c2819dbf6aee5eb6
teus <teus@2510p.(none)>
2009-01-27 17:09:20 +0200

    copyright statements updated

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,7 +13,7 @@
  **  
  ** You should have received a copy of the GNU General Public License
  ** along with this program; if not, write to the Free Software
- ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  **  
  */
 
@@ -2272,7 +2272,7 @@ void MainWindow::showabout()
                         "the Free Software Foundation; either version 3 of the License, or\n"
                         "(at your option) any later version.\n"
                         "\n"
-                        "This program is distributed in the hope that it will be useful,\n" "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" "GNU General Public License for more details.\n" "\n" "You should have received a copy of the GNU General Public License\n" "along with this program; if not, write to the Free Software\n" "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.\n", NULL);
+                        "This program is distributed in the hope that it will be useful,\n" "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" "GNU General Public License for more details.\n" "\n" "You should have received a copy of the GNU General Public License\n" "along with this program; if not, write to the Free Software\n" "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.\n", NULL);
 }
 
 void MainWindow::on_undo1_activate(GtkMenuItem * menuitem, gpointer user_data)
