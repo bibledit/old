3122173444817d95cb3bb3d15f83aae9a81cd781
Teus Benschop <teusjannette@gmail.com>
2011-12-06 20:46:19 +0100

    xiphos/bibletime: remove compiler warnings, and clearing a lot of executable bits

--- web/config.php
+++ web/config.php
@@ -14,7 +14,7 @@
 #define PACKAGE_NAME "bibledit-web"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit-web 0.5.1"
+#define PACKAGE_STRING "bibledit-web 0.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit-web"
@@ -23,7 +23,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.5.1"
+#define PACKAGE_VERSION "0.6"
 
 /* Define the password of the site administrator */
 #define SITE_ADMIN_PASSWORD "adminpassword"
@@ -32,7 +32,7 @@
 #define SITE_ADMIN_USERNAME "adminusername"
 
 /* Version number of package */
-#define VERSION "0.5.1"
+#define VERSION "0.6"
 
 /* Define the path to the Zend framework */
 #define ZENDPATH "/usr/share/php/libzend-framework-php"
