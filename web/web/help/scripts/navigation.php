2f5086ce92d9fcaea03ffc627aa3a4e118d5e6fd
Teus Benschop <teusjannette@gmail.com>
2013-07-30 16:52:37 +0200

    web: The online help about the Passage Navigator has been extended

--- web/web/help/scripts/navigation.php
+++ web/web/help/scripts/navigation.php
@@ -3,19 +3,24 @@
   <?php echo gettext ("Several pages have a Passage Navigator on top.") ?>
   <?php echo gettext ("It consists of the name of the book, the chapter, and the verse.") ?>
   <?php echo gettext ("For example:") ?>
+  <?php echo gettext ("Matthew 1:1.") ?>
 </p>
-<p>
-  <?php echo gettext ("Matthew 1:1") ?>
-</p>
+<br>
 <p>
   <?php echo gettext ("The navigator enables you to go to another passage.") ?>
   <?php echo gettext ("Click on the name of the book to navigate to another book.") ?>
   <?php echo gettext ("In the same way you can click on the chapter and on the verse.") ?>
 </p>
+<br>
 <p>
   <?php echo gettext ("You can also use the keyboard to navigate.") ?>
   <?php echo gettext ("Press Ctrl-G, enter the new passage, for example Mat 2 3, and press Enter.") ?>
+  <?php echo gettext ("You can also enter the name of the book without chapter and verse, for example, Genesis, and it will go to that book.") ?>
+  <?php echo gettext ("Or enter chapter and verse, for example 10 2, and it goes to that passage in the current book.") ?>
+  <?php echo gettext ("When entering just one number, for example 2, it will go to verse 2 in the current chapter.") ?>
+  <?php echo gettext ("Or enter Luke 10, to go to Luke chapter 10.") ?>
 </p>
+<br>
 <p>
   <?php echo gettext ("Press Escape to cancel any navigation operation.") ?>
 </p>
