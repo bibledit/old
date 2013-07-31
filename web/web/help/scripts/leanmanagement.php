eebe83bd4369f74a8fc010929acf0b93eaf85b3e
Teus Benschop <teusjannette@gmail.com>
2013-07-31 12:27:49 +0200

    web: A tiny bit of info was added about lean translation management

--- web/web/help/scripts/leanmanagement.php
+++ web/web/help/scripts/leanmanagement.php
@@ -5,6 +5,9 @@
 </p>
 <br>
 <p>
- <?php echo gettext ("...") ?>
-</p>
-<p><a href="http://site.org" target="_blank"><?php echo gettext ("External reference") ?></a></p>
+ <?php echo gettext ("Lean management is about the systematic pursuit of desired translation competence and conditions by utilizing human capabilities in a coordinated manner.") ?>
+ <?php echo gettext ("Two things are essential for this:") ?>
+<p>1. <?php echo gettext ("Developing desired conditions.") ?></p>
+<p>2. <?php echo gettext ("Developing capable people who can develop the desired conditions.") ?></p>
+<br>
+<p><a href="http://www.lean.org" target="_blank"><?php echo gettext ("External reference") ?></a></p>
