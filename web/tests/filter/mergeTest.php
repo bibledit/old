8b27a4fe5431392ec2242cf5952c14b45f2b988e
Teus Benschop <teusjannette@gmail.com>
2013-08-26 10:31:29 +0200

    web: working on character-based merge filter

--- web/tests/filter/mergeTest.php
+++ web/tests/filter/mergeTest.php
@@ -4,26 +4,15 @@
 class filterMergeTest extends PHPUnit_Framework_TestCase
 {
 
-  private $mergeBaseFile;
-  private $userModificationFile;
-  private $serverModificationFile;
-  private $outputFile;
-
   private $mergeBaseData;
   private $userModificationData;
   private $serverModificationData;
-  private $outputData;
 
 
   protected function setUp ()
   {
-    $this->mergeBaseFile = uniqid (sys_get_temp_dir() . "/");
-    $this->userModificationFile = uniqid (sys_get_temp_dir() . "/");
-    $this->serverModificationFile = uniqid (sys_get_temp_dir() . "/");
-    $this->outputFile = uniqid (sys_get_temp_dir() . "/");
     $this->tearDown ();
-
-$this->mergeBaseData = <<<'EOD'
+$usfm = <<<'EOD'
 \c 28
 \s Ukuvuka lokuzibonakalisa kukaJesu
 \p
@@ -31,55 +20,87 @@ $this->mergeBaseData = <<<'EOD'
 \v 2 Futhi khangela, kwaba khona ukuzamazama komhlaba okukhulu\x + 27.51,54.\x*; ngoba ingilosi yeNkosi yehla ivela ezulwini\x + Mark. 16.5. Luka 24.4. Joha. 20.12.\x*, yasondela yagiqa ilitshe yalisusa emnyango, yahlala phezu kwalo\x + 27.60,66.\x*.
 \v 3 Lokubonakala kwayo kwakunjengombane\x + Dan. 10.6. Hlu. 13.6.\x*, lesembatho sayo sasimhlophe njengeliqhwa elikhithikileyo\x + Dan. 7.9. Mark. 9.3.\x*.
 \v 4 Abalindi basebethuthumela ngokuyesaba, baba njengabafileyo\x + 27.65,66.\x*.
-\v 5 Kodwa ingilosi yaphendula yathi kwabesifazana: Lingesabi lina; ngoba ngiyazi ukuthi lidinga uJesu obebethelwe;
-\v 6 kakho lapha; ngoba uvukile\x + Hlu. 18.14.\x*, njengokutsho kwakhe; wozani libone indawo lapho iNkosi ebilele khona.
-\v 7 Hambani masinyane litshele abafundi bakhe ukuthi uvukile kwabafileyo\x + 27.63.\x*; njalo khangelani, uzalandulela ukuya eGalili\x + 26.32. 28.10,16.\x*; lizambona khona; khangelani, sengilitshelile.
-\p
-\v 8 Basebephuma engcwabeni ngokuphangisa belokwesaba lentokozo enkulu\x + Mark. 16.8.\x*, bagijima baya ukubikela abafundi bakhe\x + Mark. 16.8,10.\x*.
-\v 9 Kwathi besiyatshela abafundi bakhe, futhi khangela, uJesu wabahlangabeza, wathi: Mpilo enhle\x + Mark. 16.9. Joha. 20.14.\x*! Basebesondela babamba inyawo zakhe\x + 2 Kho. 4.27.\x*, bamkhonza\x + 28.17. 8.2.\x*.
-\v 10 UJesu wasesithi kubo: Lingesabi\x + 28.5.\x*; hambani, libikele abafowethu\x + Joha. 20.17. Hlab. 22.22. Roma 8.29. Heb. 2.11,12.\x* ukuthi baye eGalili, njalo bazangibona khona\x + 26.32. 28.10,16.\x*.
-\p
-\v 11 Kwathi sebehamba, khangela, abanye kubalindi baya emzini babikela abapristi abakhulu konke okwenzakeleyo\x + 27.65,66.\x*.
-\v 12 Sebebuthene labadala, benza icebo, banika abebutho imali enengi\x + 12.14.\x*,
-\v 13 besithi: Kalibokuthi: Abafundi bakhe beze ebusuku bameba silele thina.
-\v 14 Futhi uba lokhu kuzwakala kumbusi\x + 27.1.\x*, thina sizamncenga, silenze lingabi senkathazweni\x + Seb. 12.20.\x*.
-\v 15 Basebethatha imali benza njengalokhu ababekufundisiwe; lalelilizwi landa phakathi kwamaJuda\x + Mark. 1.45.\x* kuze kube lamuhla\x + 27.8.\x*.
-\s Ukulayezela kokucina kukaJesu
-\p
-\v 16 Kwathi abafundi abalitshumi lanye baya eGalili, entabeni uJesu\x + 28.7.\x* ayebalayele yona\x + 27.10.\x*;
-\v 17 sebembonile bamkhonza\x + 28.17. 8.2.\x*; kodwa abanye\x + 1 Kor. 15.6.\x* bathandabuza\x + 14.31.\x*.
-\v 18 UJesu wasesondela wakhuluma labo, esithi: Wonke amandla aphiwe mina ezulwini lemhlabeni\x + Dan. 7.13,14. Mat. 11.27; 16.28. Luka 1.32; 10.22. Joha. 3.35; 5.22,27; 13.3; 17.2. Seb. 2.36. Roma 14.9. 1 Kor. 15.27. Efe. 1.10,20-23. Fil. 2.9,10. Kol. 2.10. Heb. 1.2,6; 2.8. 1 Pet. 3.22.\x*;
-\v 19 ngakho hambani\x + Mark. 16.15,16.\x* lenze izizwe zonke zibe ngabafundi\x + Isa. 52.10. Luka 24.47. Seb. 2.39. Mat.24.14.\x*, libabhabhathiza\x + Mark. 16.15,16.\x* ebizweni likaYise leleNdodana lelikaMoya oNgcwele\x + Roma 6.3. Gal. 3.27. Seb. 8.16; 19.5. 1 Kor. 1.13; 10. 2.\x*;
-\v 20 libafundisa ukugcina konke engililaye khona; futhi khangelani, mina ngilani\x + 18.20.\x* insuku zonke kuze kube sekupheleni kwesikhathi. Ameni\x + 13.39.\x*.
 EOD;
-    file_put_contents ($this->mergeBaseFile, $this->mergeBaseData);
-
-
+    $usfm = trim ($usfm) . "\n";
+    $this->mergeBaseData = $usfm;
   }
+
   
   protected function tearDown ()
   {
-    @unlink ($this->mergeBaseFile);
-    @unlink ($this->userModificationFile);
-    @unlink ($this->serverModificationFile);
-    @unlink ($this->outputFile);
   }
   
 
-  public function testLineMergeUserModification () // Todo
+  public function testLineMergeOneModification () // Todo
   {
-    $this->userModificationData = str_replace ("Basebethatha", "bathatha", $this->mergeBaseData);
+    $this->userModificationData = str_replace ("basebethuthumela", "bathuthumela", $this->mergeBaseData);
+    $this->serverModificationData = str_replace ("28", "29", $this->mergeBaseData);
+    $output = Filter_Merge::run ($this->mergeBaseData, $this->userModificationData, $this->serverModificationData);
+    $standard = str_replace ("basebethuthumela", "bathuthumela", $this->mergeBaseData);
+    $standard = str_replace ("28", "29", $standard);
+    $this->assertEquals ($standard, $output);
+  }
+  
+  
+  public function testLineMergeUserModificationInEachLine ()
+  {
+    return;
+    $this->userModificationData = $this->mergeBaseData;
+    $this->userModificationData = str_replace ("28", "29", $this->userModificationData);
+    $this->userModificationData = str_replace ("lokuzibonakalisa", "", $this->userModificationData);
+    $this->userModificationData = str_replace ("\\p", "\\pp", $this->userModificationData);
+    $this->userModificationData = str_replace ("Kwathi", "", $this->userModificationData);
+    $this->userModificationData = str_replace ("khangela", "khangela-ke", $this->userModificationData);
+    $this->userModificationData = str_replace ("kwayo", "kwengilosi", $this->userModificationData);
+    $this->userModificationData = str_replace ("Dan. 7.9", "Danieli 7.9", $this->userModificationData);
+    $this->userModificationData = str_replace ("basebethuthumela", "bathuthumela", $this->userModificationData);
     $this->serverModificationData = $this->mergeBaseData;
-    $this->outputData = $this->userModificationData;
     file_put_contents ($this->userModificationFile, $this->userModificationData);
     file_put_contents ($this->serverModificationFile, $this->serverModificationData);
     Filter_Merge::files ($this->mergeBaseFile, $this->userModificationFile, $this->serverModificationFile, $this->outputFile);
-
-
-    //$output = file_get_contents ($this->outputFile);
-    // $this->assertEquals ($this->outputData, $output);
+    $output = file_get_contents ($this->outputFile);
+    $this->assertEquals ($this->userModificationData, $output);
   }
-  
+
+
+  public function testLineMergeOneServerModification () // Todo
+  {
+    return;
+    $this->userModificationData = $this->mergeBaseData;
+    $this->serverModificationData = str_replace ("basebethuthumela", "bathuthumela", $this->mergeBaseData);
+    file_put_contents ($this->userModificationFile, $this->userModificationData);
+    file_put_contents ($this->serverModificationFile, $this->serverModificationData);
+    Filter_Merge::files ($this->mergeBaseFile, $this->userModificationFile, $this->serverModificationFile, $this->outputFile);
+    $output = file_get_contents ($this->outputFile);
+    $this->assertEquals ($this->serverModificationData, $output);
+  }
+
+
+  public function testLineMergeServerModificationInEachLine () // Todo
+  {
+    return;
+    $this->usersModificationData = $this->mergeBaseData;
+    $this->serverModificationData = $this->mergeBaseData;
+    //$this->serverModificationData = str_replace ("28", "29", $this->serverModificationData);
+    $this->serverModificationData = str_replace ("lokuzibonakalisa", "", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("\\p", "\\pp", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("Kwathi", "", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("khangela", "khangela-ke", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("kwayo", "kwengilosi", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("Dan. 7.9", "Danieli 7.9", $this->serverModificationData);
+    //$this->serverModificationData = str_replace ("basebethuthumela", "bathuthumela", $this->serverModificationData);
+    file_put_contents ($this->userModificationFile, $this->userModificationData);
+    file_put_contents ($this->serverModificationFile, $this->serverModificationData);
+    Filter_Merge::files ($this->mergeBaseFile, $this->userModificationFile, $this->serverModificationFile, $this->outputFile);
+    $output = file_get_contents ($this->outputFile);
+    $this->assertEquals ($this->serverModificationData, $output);
+  }
+
+  public function testDie () // Todo
+  {
+    return;
+    die;
+  }  
   
   /// Todo test same modifications ok.
   
--- web/web/filter/merge.php
+++ web/web/filter/merge.php
@@ -24,22 +24,96 @@ class Filter_Merge
   /*
   This filter merges files.
   There are four arguments, all representing filenames:
-  $base: Filename with the merge base.
-  $user: Filename with the modifications made by the user.
-  $server: Filename with the modifications coming from the collaboration server.
-  $output: Fileame for the merged output.
-  The filter uses program "merge" do do a three-way merge.
-  It converts the data into a new format with one character per line, 
-  for better merging and far fewer conflicts.
-  In case of a conflict, it favours the modification from the server.
+  $base: Data for the merge base.
+  $user: Data as modified by the user.
+  $server: Data as modified by the collaboration server.
+  The filter uses program "git" do do a three-way merge.
+  If necessary it converts the data into a new format with one character per line, 
+  for more fine-grained merging.
+  In case of a conflict, it favours the edition from the server.
   */
-  public static function files ($base, $user, $server, $output) // Todo
+  public static function run ($base, $user, $server) // Todo
   {
     // First try a standard line-based merge. Should be sufficient for most cases.
+    $repository = Filter_Merge::createRepository ();    
+    $userClone = Filter_Merge::cloneRepository ($repository);
+    Filter_Merge::commitData ($userClone, $base);
+    Filter_Merge::pushData ($userClone);
+    $serverClone = Filter_Merge::cloneRepository ($repository);
+    Filter_Merge::commitData ($serverClone, $server);
+    Filter_Merge::pushData ($serverClone);
+    Filter_Merge::commitData ($userClone, $user);
+    $exit_code = Filter_Merge::pullData ($userClone);
+    var_dump ($exit_code); // Todo
+    $result = file_get_contents ("$userClone/data");
+    if ($exit_code == 0) {
+
+    }
+
+    Filter_Rmdir::rmdir ($repository);
+    // Todo Filter_Rmdir::rmdir ($userClone);
+    Filter_Rmdir::rmdir ($serverClone);
+        
+    return $result;
+
+
+
 
 
   }
+
+
+  // This function creates a file-based git repository.
+  // It returns the path to the repository.
+  private static function createRepository ()
+  {
+    $repository = uniqid (sys_get_temp_dir () . "/") . "repository";
+    mkdir ($repository);
+    $command = "cd $repository; git --bare init --shared 2>&1";
+    exec ($command, $output, $exit_code);
+    return $repository;
+  }
+
+
+  // This function clones a git $repository,
+  // It returns the path to the clone.
+  private static function cloneRepository ($repository)
+  {
+    $clone = uniqid (sys_get_temp_dir() . "/") . "clone";
+    $command = "git clone file://$repository $clone 2>&1";
+    exec ($command, $output, $exit_code);
+    return $clone;
+  }
+
   
+  // The function commit $data to the git repository at $clone.
+  private static function commitData ($clone, $data)
+  {
+    file_put_contents ("$clone/data", $data);
+    $command = "cd $clone; git add . 2>&1";
+    exec ($command, $output, $exit_code);
+    $command = "cd $clone; git commit -a -m commit 2>&1";
+    exec ($command, $output, $exit_code);
+  }
+
+   
+  // The function pushes data in $clone to its server.
+  private static function pushData ($clone)
+  {
+    $command = "cd $clone; git push 2>&1";
+    exec ($command, $output, $exit_code);
+  }
+
+
+  // The function pulls data for $clone from its server.
+  // It returns the exit code of the process.
+  private static function pullData ($clone)
+  {
+    $command = "cd $clone; git pull 2>&1";
+    exec ($command, $output, $exit_code);
+    return $exit_code;
+  }
+
 
 }
 
