3887ba98cfc69b6386fad693aea38d82b17ad112
Teus Benschop <teusjannette@gmail.com>
2013-09-16 09:14:27 +0200

    web: A <style> was removed from the Consistency page, because it is not used

--- web/web/consistency/scripts/index.php
+++ web/web/consistency/scripts/index.php
@@ -17,12 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 ?>
-<style type="text/css">
-  *.searchResult {
-      font-weight: bold;
-      background-color: yellow;
-  }
-</style>
 <h3><?php echo gettext ("Consistency") ?></h3>
 <p><a href="#help">[<?php echo gettext ("help") ?>]</a></p>
 <br>
