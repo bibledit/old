75fac65311748e7022c106c821d68925cd984cae
Teus Benschop <teusjannette@gmail.com>
2013-09-16 09:39:59 +0200

    web: Changes made in the Bible text online are stored in the database for later processing

--- web/web/editusfm/save.php
+++ web/web/editusfm/save.php
@@ -16,14 +16,25 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
+
 require_once ("../bootstrap/bootstrap.php");
 page_access_level (TRANSLATOR_LEVEL);
+
+
 $database_config_user = Database_Config_User::getInstance ();
 $database_logs = Database_Logs::getInstance ();
+$database_bibles = Database_Bibles::getInstance ();
+$database_changesuser = Database_ChangesUser::getInstance ();
+$session_logic = Session_Logic::getInstance ();
+
+
 $bible = $_POST['bible'];
 $book = $_POST['book'];
 $chapter = $_POST['chapter'];
 $usfm = $_POST['usfm'];
+
+
 if (isset ($bible) && isset ($book) && isset ($chapter) && isset ($usfm)) {
   $usfm = trim ($usfm);
   if ($usfm != "") {
@@ -35,8 +46,17 @@ if (isset ($bible) && isset ($book) && isset ($chapter) && isset ($usfm)) {
         $chapter_number = $data[1];
         $chapter_data_to_save = $data[2];
         if ((($book_number == $book) || ($book_number == 0)) && ($chapter_number == $chapter)) {
+          // Collect some data about the changes for this user.
+          $username = $session_logic->currentUser ();
+          $oldID = $database_bibles->getChapterId ($bible, $book, $chapter);
+          $oldText = $database_bibles->getChapter ($bible, $book, $chapter);
+          // Safely store the chapter.
           $saved = Filter_Bibles::safeStoreChapter ($bible, $book, $chapter, $chapter_data_to_save);
           if ($saved) {
+            // Store details for the user's changes.
+            $newID = $database_bibles->getChapterId ($bible, $book, $chapter);
+            $newText = $chapter_data_to_save;
+            $database_changesuser->recordSave ($username, $bible, $book, $chapter, $oldID, $oldText, $newID, $newText);
             echo gettext ("Saved");
           } else {
             echo gettext ("Not saved because of too many changes");
@@ -57,4 +77,6 @@ if (isset ($bible) && isset ($book) && isset ($chapter) && isset ($usfm)) {
 } else {
   echo gettext ("Nothing to save");
 }
+
+
 ?>
