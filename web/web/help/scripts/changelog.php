38def511cad39d2cd46e1ecad7156f91e9031293
Teus Benschop <teusjannette@gmail.com>
2013-09-21 16:50:17 +0200

    web: The logbook adds the day of the sprint history being updated

--- web/web/help/scripts/changelog.php
+++ web/web/help/scripts/changelog.php
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 <p>The tool to remove duplicate consultation notes was renamed to "Clean Consultation Notes".</p>
 <p>The tool to clean the consultation notes converts the note summaries to clean text.</p>
 <p>Users can opt for having their personal change proposals visible among the Change Notifications online.</p>
-<p></p>
+<p>The logbook shows for which day of the month the sprint history gets updated.</p>
 <p></p>
 <p></p>
 <p></p>
