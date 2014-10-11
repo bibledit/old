9e09fbfc20e4deae3b3feeda2aa35a364a0d7485
Teus Benschop <teusjannette@gmail.com>
2014-10-11 22:44:44 +0200

    Include SQLite source

---
+++ lib/webserver/http.cpp
@@ -0,0 +1,89 @@
+/*
+Copyright (©) 2003-2014 Teus Benschop.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#include <webserver/http.h>
+#include <vector>
+#include <sstream>
+
+
+using namespace std;
+
+
+
+/*
+The http headers from a browser could look as follows:
+ 
+GET /index/page HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36
+Accept-Language: sn,en-US;q=0.8,en;q=0.6
+
+The function extracts the page request:
+
+/index/page
+
+*/
+string http_get_header_get (string headers)
+{
+  string get = "/index";
+  istringstream iss(headers);
+  string s;
+  bool bingo = false;
+  while (getline (iss, s, ' ')) 
+  {
+    if (bingo) {
+      get = s;
+      bingo = false;
+    }
+    if (s == "GET") bingo = true;
+  }
+  return get;
+}
+
+
+/*
+
+The function assembles the response to be given to the browser.
+code: an integer response code, normally this is 200.
+contents: the response body to be sent.
+
+The function inserts the correct headers,
+and returns the entire result to be sent back to the browser.
+
+*/
+string http_assemble_response (int code, string contents)
+{
+  ostringstream length;
+  length << contents.size ();
+
+  vector <string> response;
+  response.push_back ("HTTP/1.1 200 OK");
+  response.push_back ("Content-Length: " + length.str());
+  response.push_back ("Content-Type: text/html;charset=UTF-8");
+  response.push_back ("");
+  response.push_back (contents);
+
+  string assembly;
+  for (unsigned int i = 0; i < response.size (); i++) {
+    if (i > 0) assembly += "\n";
+    assembly += response [i];
+  }
+  return assembly;
+}
---
+++ lib/webserver/webserver.cpp
@@ -0,0 +1,103 @@
+/*
+Copyright (©) 2003-2014 Teus Benschop.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <webserver/http.h>
+
+
+using namespace std;
+
+
+void webserver () 
+{
+  // Create a socket descriptor.
+  int listenfd = socket (AF_INET, SOCK_STREAM, 0);
+  int optval = 1;
+
+  // Eliminate "Address already in use" error from bind.
+  setsockopt (listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval, sizeof (int));
+  
+  // Listenfd will be an endpoint for all requests to a port on any IP address for this host.
+  typedef struct sockaddr SA;
+  struct sockaddr_in serveraddr;
+  memset (&serveraddr, 0, sizeof (serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)8080);
+  bind (listenfd, (SA *) &serveraddr, sizeof (serveraddr));
+      
+  // Make it a listening socket ready to accept many connection requests.
+  listen (listenfd, 100);
+  
+  // Ignore SIGPIPE signal: When the browser cancels the request, it won't kill the app.
+  signal(SIGPIPE, SIG_IGN);
+
+  // Keep waiting for, accepting, and processing connections.
+  while (true) {
+      
+    // Socket and file descriptor for the client connection.
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen = sizeof (clientaddr);
+    int connfd = accept (listenfd, (SA *) &clientaddr, &clientlen);
+    // &clientaddr contains client info.
+
+    // Read the client's request.
+    char buffer[65535];
+    size_t bytes_read;
+    bytes_read = read (connfd, buffer, sizeof (buffer));
+    if (bytes_read);
+    string request = buffer;
+
+    // The requested page.
+    string get = http_get_header_get (request);
+
+    // Assemble response.
+    string reply = "<h1>Bibledit-Web</h1>";
+    reply = http_assemble_response (200, reply);
+
+    // Send response to browser.    
+    const char * output = reply.c_str();
+    size_t length = strlen (output);
+    ssize_t written = write (connfd, output, length);
+    if (written);
+
+    // Done: Close.
+    close (connfd);
+  }
+
+
+}
+
