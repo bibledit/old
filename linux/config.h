6abc77127ce30bc831e3524fd8cb7d9e1ca8d3eb
Teus Benschop <teusjannette@gmail.com>
2014-10-28 16:15:36 +0100

    Bibledit-Linux: Single instance app

---
+++ linux/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "bibledit-linux"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.nongnu.org/bibledit"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "bibledit-linux"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "bibledit-linux 1.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "bibledit-linux"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.1"
+
+/* Version number of package */
+#define VERSION "1.0.1"
