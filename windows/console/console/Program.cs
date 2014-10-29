d59e9c9edd1b2085483790c63fa86c83c4c86426
Teus Benschop <teusjannette@gmail.com>
2014-10-29 12:08:35 +0100

    Windows console app calls libbibledit.dll function

--- windows/console/console/Program.cs
+++ windows/console/console/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace console
 {
     class Program
     {
+        
+        [DllImport("libbibledit.dll")]
+        public static extern void bibledit_run();
+
         static void Main(string[] args)
         {
+            bibledit_run();
+
         }
     }
 }
