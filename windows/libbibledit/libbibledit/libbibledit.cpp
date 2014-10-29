579754aa2b387763618c4ac95a1bdcc0da222e30
Teus Benschop <teusjannette@gmail.com>
2014-10-29 11:30:13 +0100

    libbibledit.dll solution for Windows was added

---
+++ windows/libbibledit/libbibledit/libbibledit.cpp
@@ -0,0 +1,18 @@
+// libbibledit.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "libbibledit.h"
+#include <iostream>
+#include <stdexcept>
+
+
+using namespace std;
+
+
+void bibledit_run()
+{
+	cout << "Hello from libbibledit WIN32" << endl;
+}
+
+
---
+++ windows/libbibledit/libbibledit/libbibledit.h
@@ -0,0 +1,8 @@
+// libbibledit.h
+
+#include <stdio.h>
+
+extern "C"
+{
+	__declspec(dllexport) void bibledit_run();
+}
