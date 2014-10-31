5091358e4002ebf8d25890dd027d08aa439e04eb
Teus Benschop <teusjannette@gmail.com>
2014-10-31 16:55:23 +0100

    WIN32

--- lib/database/config/general.cpp
+++ lib/database/config/general.cpp
@@ -18,12 +18,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 #include <database/config/general.h>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <libgen.h>
-#include <sys/stat.h>
-#include <cstring>
 #include <filter/url.h>
 #include <filter/string.h>
 
