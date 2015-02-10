6efc32ec2d72b5bff498d5807196e24c3739d28f
Teus Benschop <teusjannette@gmail.com>
2015-02-10 15:42:07 +0100

    Handles more http response codes.

--- lib/filter/url.cpp
+++ lib/filter/url.cpp
@@ -485,24 +485,53 @@ string filter_url_http_response_code_text (int code)
   string text = convert_to_string (code);
   text.append (" ");
   switch (code) {
-    case 200:
-      text += "OK";
-      break;
-    case 302:
-      text += "Found";
-      break;
-    case 400:
-      text += "Bad Request";
-      break;
-    case 401:
-      text += "Unauthorized";
-      break;
-    case 409:
-      text += "Conflict";
-      break;
-    default:
-      text += "Error";
-      break;
+    case 100: text += "Continue"; break;
+    case 101: text += "Switching Protocols"; break;
+    case 200: text += "OK"; break;
+    case 201: text += "Created"; break;
+    case 202: text += "Accepted"; break;
+    case 203: text += "Non-Authoritative Information"; break;
+    case 204: text += "No Content"; break;
+    case 205: text += "Reset Content"; break;
+    case 206: text += "Partial Content"; break;
+    case 300: text += "Multiple Choices"; break;
+    case 301: text += "Moved Permanently"; break;
+    case 302: text += "Found"; break;
+    case 303: text += "See Other"; break;
+    case 304: text += "Not Modified"; break;
+    case 305: text += "Use Proxy"; break;
+    case 307: text += "Temporary Redirect"; break;
+    case 308: text += "Permanent Redirect"; break;
+    case 400: text += "Bad Request"; break;
+    case 401: text += "Unauthorized"; break;
+    case 402: text += "Payment Required"; break;
+    case 403: text += "Forbidden"; break;
+    case 404: text += "Not Found"; break;
+    case 405: text += "Method Not Allowed"; break;
+    case 406: text += "Not Acceptable"; break;
+    case 407: text += "Proxy Authentication Required"; break;
+    case 408: text += "Request Timeout"; break;
+    case 409: text += "Conflict"; break;
+    case 410: text += "Gone"; break;
+    case 411: text += "Length Required"; break;
+    case 412: text += "Precondition Failed"; break;
+    case 413: text += "Request Entity Too Large"; break;
+    case 414: text += "Request-URI Too Long"; break;
+    case 415: text += "Unsupported Media Type"; break;
+    case 416: text += "Requested Range Not Satisfiable"; break;
+    case 417: text += "Expectation Failed"; break;
+    case 426: text += "Upgrade Required"; break;
+    case 428: text += "Precondition Required"; break;
+    case 429: text += "Too Many Requests"; break;
+    case 431: text += "Request Header Fields Too Large"; break;
+    case 500: text += "Internal Server Error"; break;
+    case 501: text += "Not Implemented"; break;
+    case 502: text += "Bad Gateway"; break;
+    case 503: text += "Service Unavailable"; break;
+    case 504: text += "Gateway Timeout"; break;
+    case 505: text += "HTTP Version Not Supported"; break;
+    case 511: text += "Network Authentication Required"; break;
+    default:  text += "Error"; break;
   }
   return text;
 }
