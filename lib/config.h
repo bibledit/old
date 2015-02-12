3fc5fb8b6c20736f8d93381ceb9785f2d85dcdcb
Teus Benschop <teusjannette@gmail.com>
2015-02-12 14:36:43 +0100

    lib: configure.ac detects libgit2.* in srcdir.

--- lib/config.h
+++ lib/config.h
@@ -43,9 +43,6 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* The path to libgit2 */
-#define LIBGIT_PATH "undefined"
-
 /* Define the network port number */
 #define NETWORK_PORT "8080"
 
@@ -59,7 +56,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 1.0.64"
+#define PACKAGE_STRING "bibledit 1.0.65"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -68,10 +65,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.64"
+#define PACKAGE_VERSION "1.0.65"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.64"
+#define VERSION "1.0.65"
