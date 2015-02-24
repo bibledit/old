76de6790e047bb2a66d396c82a2573ab5ee97adb
Teus Benschop <teusjannette@gmail.com>
2015-02-24 07:22:36 +0100

    iOS - library sync

--- lib/config.h
+++ lib/config.h
@@ -56,7 +56,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 1.0.74"
+#define PACKAGE_STRING "bibledit 1.0.75"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -65,16 +65,16 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.74"
+#define PACKAGE_VERSION "1.0.75"
 
 /* Define the maximum number of parallel background tasks to run */
 #define PARALLEL_TASKS 10
 
 /* Define whether the browser has only one tab for opening pages */
-#define SINGLE_TAB "yes"
+#define SINGLE_TAB "no"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.74"
+#define VERSION "1.0.75"
