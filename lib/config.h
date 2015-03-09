208637863eca93078b0e43594d9ee199fd7a3a5a
Teus Benschop <teusjannette@gmail.com>
2015-03-09 12:50:07 +0100

    Newer autotools for Bibledit-Gtk.

--- lib/config.h
+++ lib/config.h
@@ -71,7 +71,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 1.0.86"
+#define PACKAGE_STRING "bibledit 1.0.87"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -80,7 +80,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.86"
+#define PACKAGE_VERSION "1.0.87"
 
 /* Define the maximum number of parallel background tasks to run */
 #define PARALLEL_TASKS 10
@@ -89,4 +89,4 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.86"
+#define VERSION "1.0.87"
