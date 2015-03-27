555e355a917761230716425bca2be7321fddb8a0
Teus Benschop <teusjannette@gmail.com>
2015-03-27 14:55:58 +0100

    Initial version of versification check.

--- lib/unittests/tests/checks/versificationTest.php
+++ lib/unittests/tests/checks/versificationTest.php
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
-class versificationsTest extends PHPUnit_Framework_TestCase
+class versificationsTest extends PHPUnit_Framework_TestCase // Todo
 {
 
 
