e057a7e957e670204281a3c56246fb1181be3cfb
Teus Benschop <teusjannette@gmail.com>
2015-04-08 20:13:09 +0200

    Initial Bibledit OS X GUI

---
+++ osx/Bibledit/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Bibledit
+//
+//  Created by Teus Benschop on 08/04/2015.
+//  Copyright (c) 2015 Teus Benschop. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ osx/Bibledit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bibledit
+//
+//  Created by Teus Benschop on 08/04/2015.
+//  Copyright (c) 2015 Teus Benschop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
