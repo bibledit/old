02de99fd03e9d1d093c8b17690267e23788a68b5
Teus Benschop <teusjannette@gmail.com>
2015-05-22 20:38:16 +0200

    Porting to Android ...

---
+++ android/README.md
@@ -0,0 +1,7 @@
+# Bibledit for Android
+
+## Developer information
+
+To get a shell on the device: $ adb shell
+
+To view the logbook from within the shell on the device: $ logcat
--- android/gen/R.java.d
+++ android/gen/R.java.d
@@ -1,8 +1,12 @@
-/home/teus/documents/dev/android/gen/org/bibledit/android/R.java \
- : /home/teus/documents/dev/android/res/drawable-mdpi/ic_launcher.png \
-/home/teus/documents/dev/android/res/drawable-xhdpi/ic_launcher.png \
-/home/teus/documents/dev/android/res/drawable-ldpi/ic_launcher.png \
-/home/teus/documents/dev/android/res/layout/main.xml \
-/home/teus/documents/dev/android/res/values/strings.xml \
-/home/teus/documents/dev/android/res/drawable-hdpi/ic_launcher.png \
-/home/teus/documents/dev/android/bin/AndroidManifest.xml \
+/Users/teus/Documents/dev/bibledit/android/gen/org/bibledit/android/R.java \
+ : /Users/teus/Documents/dev/bibledit/android/res/drawable-hdpi/bibledit512x512.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-hdpi/ic_launcher.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-ldpi/bibledit512x512.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-ldpi/ic_launcher.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-mdpi/bibledit512x512.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-mdpi/ic_launcher.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-xhdpi/bibledit512x512.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/drawable-xhdpi/ic_launcher.jpg \
+/Users/teus/Documents/dev/bibledit/android/res/layout/main.xml \
+/Users/teus/Documents/dev/bibledit/android/res/values/strings.xml \
+/Users/teus/Documents/dev/bibledit/android/bin/AndroidManifest.xml \
