173ba3b7be32ad4c491c9fd06a92f80aee6d7f56
Teus Benschop <teusjannette@gmail.com>
2015-05-30 21:02:22 +0200

    Android port ...

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,16 +1,14 @@
-# Tool chain to use.
-# NDK_TOOLCHAIN_VERSION := 4.8
-
-# Which processors to build for.
-APP_ABI := all
-# APP_ABI := armeabi x86
+# Which processor architectures to build for.
+# APP_ABI := all
+# APP_ABI := x86
 # APP_ABI := armeabi
+APP_ABI := armeabi-v7a
 
 #  Enable C++11 and exceptions
 APP_CPPFLAGS += -std=c++11 -fexceptions
 
 # Instruct to use the static GNU STL implementation
-APP_STL := gnustl_static
-# APP_STL := stlport_static
+# http://www.kandroid.org/ndk/docs/CPLUSPLUS-SUPPORT.html
+APP_STL := gnustl_shared
 
 LOCAL_C_INCLUDES += ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include
