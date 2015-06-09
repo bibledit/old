5845d99ae6a65e5c1aef8f952a5ed51f9e266aad
Teus Benschop <teusjannette@gmail.com>
2015-06-09 10:53:19 +0200

    Cleanup iOS port

--- lib/config.h
+++ lib/config.h
@@ -83,7 +83,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 1.0.155"
+#define PACKAGE_STRING "bibledit 1.0.156"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -92,7 +92,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.155"
+#define PACKAGE_VERSION "1.0.156"
 
 /* Define the maximum number of parallel background tasks to run */
 #define PARALLEL_TASKS 10
@@ -107,4 +107,4 @@
 #define URLSETTINGS "no"
 
 /* Version number of package */
-#define VERSION "1.0.155"
+#define VERSION "1.0.156"
