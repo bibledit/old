6cca8612254f1ad6dfaef36eebc77311c2503d6b
Teus Benschop <teusjannette@gmail.com>
2015-07-11 10:38:05 +0200

    Updated workbench / iOS docs.

--- lib/config.h
+++ lib/config.h
@@ -89,7 +89,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 1.0.183"
+#define PACKAGE_STRING "bibledit 1.0.184"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -98,7 +98,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.183"
+#define PACKAGE_VERSION "1.0.184"
 
 /* Define the maximum number of parallel background tasks to run */
 #define PARALLEL_TASKS 10
@@ -119,4 +119,4 @@
 #define URLSETTINGS "no"
 
 /* Version number of package */
-#define VERSION "1.0.183"
+#define VERSION "1.0.184"
