370c67646baebb5e88a8c65d849155998383c44c
Teus Benschop <teusjannette@gmail.com>
2015-08-18 11:59:04 +0200

    Better checks suppression handling

--- lib/unittests/unittest.cpp
+++ lib/unittests/unittest.cpp
@@ -279,7 +279,7 @@ int main (int argc, char **argv)
   // Flag for unit tests.
   config_globals_unit_testing = true;
   
-  // test_database_morphology (); refresh_sandbox (true); exit (0);
+  test_database_check (); refresh_sandbox (true); exit (0); // Todo
   
   // Run the tests.
   test_database_config_general ();
