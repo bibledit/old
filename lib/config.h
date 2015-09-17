8827f8dd49698f161264c155625c2c7076db0e70
Teus Benschop <teusjannette@gmail.com>
2015-09-17 12:47:49 +0200

    Before linking a resource to the central cache, it checks whether the resource is available from that cache

--- lib/config.h
+++ lib/config.h
@@ -113,7 +113,7 @@
 #define PARATEXT "no"
 
 /* Define the path to the shared external resources cache */
-#define RESOURCES_PATH ""
+#define RESOURCES_PATH "/Users/teus/bibledit/server/databases/offlineresources"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
