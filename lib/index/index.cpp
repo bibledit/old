d2220da41d5707670c3ada3e44642d6676487771
Teus Benschop <teusjannette@gmail.com>
2015-09-18 17:26:23 +0200

    Login and logout and ACL improvements

--- lib/index/index.cpp
+++ lib/index/index.cpp
@@ -21,6 +21,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <assets/view.h>
 #include <assets/page.h>
 #include <filter/roles.h>
+#include <filter/string.h>
+#include <locale/translate.h>
+#include <edit/index.h>
+#include <notes/index.h>
+#include <resource/index.h>
+#include <changes/changes.h>
 
 
 const char * index_index_url ()
@@ -37,11 +43,32 @@ bool index_index_acl (void * webserver_request)
 
 string index_index (void * webserver_request)
 {
+  Webserver_Request * request = (Webserver_Request *) webserver_request;
+  
   string page;
 
   page = Assets_Page::header ("Bibledit", webserver_request);
 
   Assets_View view = Assets_View ();
+  
+  vector <string> menublock;
+  if (request->session_logic ()->loggedIn ()) {
+    menublock.push_back ("<p>" + translate ("What would you like to do?") + "</p>");
+    if (edit_index_acl (request)) {
+      menublock.push_back ("<p><a href=\"/" + edit_index_url () + "\">" + translate ("Edit Bible text") + "</a></p>");
+    }
+    if (notes_index_acl (request)) {
+      menublock.push_back ("<p><a href=\"/" + notes_index_url () + "\">" + translate ("View notes") + "</a></p>");
+    }
+    if (resource_index_acl (request)) {
+      menublock.push_back ("<p><a href=\"/" + resource_index_url () + "\">" + translate ("View resources") + "</a></p>");
+    }
+    if (changes_changes_acl (request)) {
+      menublock.push_back ("<p><a href=\"/" + changes_changes_url () + "\">" + translate ("View changes") + "</a></p>");
+    }
+  }
+  view.set_variable ("menublock", filter_string_implode (menublock, "\n"));
+  
   page += view.render ("index", "index");
 
   page += Assets_Page::footer ();
