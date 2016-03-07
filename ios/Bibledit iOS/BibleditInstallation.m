22f59952441794a0686b3037ad77410e26fd3e87
Teus Benschop <teusjannette@gmail.com>
2016-03-07 19:43:26 +0100

    Fixed: 2.23 - Apps must follow the iOS Data Storage Guidelines or they will be rejected
    2.23 - Apps must follow the iOS Data Storage Guidelines or they will be
    rejected

--- ios/Bibledit iOS/BibleditInstallation.m
+++ ios/Bibledit iOS/BibleditInstallation.m
@@ -17,33 +17,55 @@
 
 + (void) run
 {
-    // Bail out in case the installed version equals the bibledit library version.
-    NSString * version = [self libraryVersion];
-    if ([version isEqualToString:[self installedVersion]]) return;
-    
-    // Wait for some time till the Bibledit library has installed its resources.
-    [NSThread sleepForTimeInterval:90];
-    //NSLog(@"%s", "bibleditInstallResources after delay");
+    // Disable the entire webroot folder from being backed up to iCloud.
+    // The reason is that if it were included,
+    // the size of the backup would be larger than what Apple allows.
 
+    /*
+    2.23 - Apps must follow the iOS Data Storage Guidelines or they will be rejected
+    Hello,
+    
+    We are writing to let you know about new information regarding your app, Bibledit, version 1.0.377, currently live on the App Store.
+    
+    Upon re-evaluation, we found that your app is not in compliance with the App Store Review Guidelines. Specifically, we found:
+    
+    2.23
+    On launch and content download, your app stores 89.43 MB on the user's iCloud, which does not comply with the iOS Data Storage Guidelines.
+    
+    Please verify that only the content that the user creates using your app, e.g., documents, new files, edits, etc. is backed up by iCloud as required by the iOS Data Storage Guidelines. Also, check that any temporary files used by your app are only stored in the /tmp directory; please remember to remove or delete the files stored in this location when it is determined they are no longer needed.
+    
+    Data that can be recreated but must persist for proper functioning of your app - or because users expect it to be available for offline use - should be marked with the "do not back up" attribute. For NSURL objects, add the NSURLIsExcludedFromBackupKey attribute to prevent the corresponding file from being backed up. For CFURLRef objects, use the corresponding kCRUFLIsExcludedFromBackupKey attribute.
+    
+    To check how much data your app is storing:
+    
+    - Install and launch your app
+    - Go to Settings > iCloud > Storage > Manage Storage
+    - Select your device
+    - If necessary, tap "Show all apps"
+    - Check your app's storage
+    
+    For additional information on preventing files from being backed up to iCloud and iTunes, see Technical Q&A 1719: How do I prevent files from being backed up to iCloud and iTunes.
+    
+    To ensure there is no interruption of the availability of your app on the App Store, please submit an update within two weeks of the date of this message. If we do not receive an update within two weeks, your app may be removed from sale.
+    
+    If you have any questions about this information, please reply to this message to let us know.
+    
+    Best regards,
+    
+    App Store Review
+    */
+    
+    [NSThread sleepForTimeInterval:2];
     NSString * webroot = [BibleditPaths documents];
-    
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSDirectoryEnumerator *directoryEnumerator = [fileManager enumeratorAtPath:webroot];
-    NSString *file;
-    BOOL isDir;
-    while (file = [directoryEnumerator nextObject]) {
-        NSArray * components = [NSArray arrayWithObjects:webroot, file, nil];
-        NSString * path = [NSString pathWithComponents:components];
-        if ([fileManager fileExistsAtPath:path isDirectory:&isDir]) {
-            // Exclude path from the iCloud backup.
-            NSString * schemePath = [NSString stringWithFormat:@"file://%@", path];
-            NSURL *url = [[NSURL alloc] initWithString:schemePath];
-            [url setResourceValue: [NSNumber numberWithBool: YES] forKey: NSURLIsExcludedFromBackupKey error: nil];
-        }
+    //NSLog(@"%@", webroot);
+    NSURL* URL= [NSURL fileURLWithPath: webroot];
+    NSError *error = nil;
+    BOOL success = [URL setResourceValue: [NSNumber numberWithBool: YES]
+                                  forKey: NSURLIsExcludedFromBackupKey error: &error];
+    if(!success){
+        //NSLog(@"Error excluding %@ from backup %@", [URL lastPathComponent], error);
     }
-    
-    // Store the installed version in the preferences.
-    [self installedVersion:version];
+    //NSLog (@"%d", success);
 }
 
 
