dd1b3b1bf696a14109bb0c492d29bfefe8e99a3d
Teus Benschop <teusjannette@gmail.com>
2016-05-31 19:55:55 +0200

    Windows builder automation in progress...

--- windows/build.bat
+++ windows/build.bat
@@ -1,6 +1,13 @@
+echo Build script from within Windows.
 
+echo Setting the path so it can also find the Cygwin binaries.
 set PATH=C:\cygwin\bin;%PATH%
 
-echo %PATH%
+echo Deleting possible existing Bibledit installation.
+del /F /S /Q C:\bibledit
 
+echo Building the Bibledit core library from within Cygwin bash.
 bash /cygdrive/e/dev/bibledit/windows/build.sh
+
+echo Copying the result.
+robocopy C:\bibledit E:\bibledit-windows /MIR
