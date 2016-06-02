d9f71b73653908cde1b7f247c6f11000504927ba
Teus Benschop <teusjannette@gmail.com>
2016-06-02 13:27:19 +0200

    The Windows builder script now compiles the bibleditsetup.exe

--- windows/build.bat
+++ windows/build.bat
@@ -9,5 +9,14 @@ del /F /S /Q C:\bibledit
 echo Building the Bibledit core library from within Cygwin bash.
 bash /cygdrive/e/dev/bibledit/windows/build.sh
 
+echo Copy the Bibledit Windows GUI into place.
+copy /Y C:\bibledit.exe\bibledit.exe C:\bibledit
+
+echo Create the bibleditsetup.exe.
+E:
+cd \
+cd dev\bibledit\windows
+"C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc package.iss
+
 echo Copying the result.
 robocopy C:\bibledit E:\bibledit-windows /MIR
