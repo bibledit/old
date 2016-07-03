008784a7baa04537d1bafabe6ce0d0df4b0916f1
Teus Benschop <teusjannette@gmail.com>
2016-07-03 17:53:10 +0200

    Paratext configuration code refactored

--- lib/help/install.sh
+++ lib/help/install.sh
@@ -170,7 +170,7 @@ fi
 $DRYECHO cd bibledit
 # Remove bits from any older build that might cause crashes in the new build.
 find . -name "*.o" -delete
-$DRYECHO ./configure --enable-paratext --enable-linux
+$DRYECHO ./configure --enable-linux
 if [ $? -ne 0 ]
 then
   echo Failed to configure Bibledit
