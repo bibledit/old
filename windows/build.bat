6776cb3c002fffa3237d68f00e2704eaab1050d5
Teus Benschop <teusjannette@gmail.com>
2016-07-12 11:16:33 +0200

    Making the Windows builder less wordy...

--- windows/build.bat
+++ windows/build.bat
@@ -4,7 +4,7 @@ echo Setting the path so it can also find the Cygwin binaries.
 set PATH=C:\cygwin\bin;%PATH%
 
 echo Deleting possible existing Bibledit installation.
-del /F /S /Q C:\bibledit
+del /F /S /Q C:\bibledit > nul
 
 echo Building the Bibledit core library from within Cygwin bash.
 bash /cygdrive/e/dev/bibledit/windows/build.sh
@@ -19,4 +19,4 @@ cd dev\bibledit\windows
 "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc package.iss
 
 echo Copying the result.
-robocopy C:\bibledit E:\bibledit-windows /MIR
+robocopy C:\bibledit E:\bibledit-windows /MIR > nul
