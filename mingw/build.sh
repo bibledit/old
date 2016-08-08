a13486f0fdaeb6eae7f62a4eaccf47a32c4218e9
Teus Benschop <teusjannette@gmail.com>
2016-08-08 17:07:20 +0200

    Cleaner Bibledit source tree
    There’s no a ‘windows’ folder for building Bibledit through cygwin, and
    a ‘mingw’ folder for building it through MinGW.

--- mingw/build.sh
+++ mingw/build.sh
@@ -13,20 +13,21 @@ TEMPDIR=/tmp/bibledit-windows
 
 echo Synchronizing relevant source code to the temporal directory.
 mkdir -p $TEMPDIR
-rsync --archive --delete --exclude '.deps' --exclude '*.o' --exclude '*.a' --exclude '.dirstamp' --exclude 'server' --exclude 'unittest' --exclude '.DS_Store' ../../lib $TEMPDIR/
-# rsync --archive --delete --exclude '.DS_Store'../native $TEMPDIR/
+rsync --archive --delete --exclude '.deps' --exclude '*.o' --exclude '*.a' --exclude '.dirstamp' --exclude 'server' --exclude 'unittest' --exclude '.DS_Store' ../lib $TEMPDIR/
+# rsync --archive --delete --exclude '.DS_Store'../mingw $TEMPDIR/
 
 
 cd $TEMPDIR/lib
 
 
 # Fix g++.exe: error: unrecognized command line option '-rdynamic'
-# Fix undefined reference to `_imp__*'
+# Fix undefined reference to `_imp__*' by adding required library to the linker.
 sed -i 's/-rdynamic/-lws2_32/' Makefile.am
+
 
 # Limit the maximum number of parallel background tasks 
 # to make the interface more responsive.
-./configure --enable-windows --with-network-port=9876 --enable-msys
+./configure --enable-windows --with-network-port=9876 --enable-mingw
 EXIT_CODE=$?
 if [ $EXIT_CODE != 0 ]; then
   exit
