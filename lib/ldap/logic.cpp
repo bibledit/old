93722ce4a0a26af3f22497d97a6a131f708f1a3d
Teus Benschop <teusjannette@gmail.com>
2016-10-10 13:53:21 +0200

    Refactoring LDAP configuration storageq
    #127

---
+++ lib/ldap/logic.cpp
@@ -0,0 +1,31 @@
+/*
+ Copyright (©) 2003-2016 Teus Benschop.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#include <ldap/logic.h>
+
+
+// These global variables contain the settings for the OpenLDAP server
+// to query for user credentials.
+string ldap_logic_uri;
+string ldap_logic_binddn;
+string ldap_logic_basedn;
+string ldap_logic_scope;
+string ldap_logic_filter;
+
+
