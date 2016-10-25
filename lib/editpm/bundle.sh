811c5e2173eee75f46516a9729b686ea438e553c
Teus Benschop <teusjannette@gmail.com>
2016-10-25 20:39:42 +0200

    Basic ProseMirror editor
    The editor was implemented according to
    https://prosemirror.net/guide/basics.html. #97

--- lib/editpm/bundle.sh
+++ lib/editpm/bundle.sh
@@ -6,3 +6,12 @@ cp ./node_modules/prosemirror-view/style/prosemirror.css .
 rm main.js
 mv *.js ~/dev/bibledit/lib/editpm/
 mv *.css ~/dev/bibledit/lib/editpm/
+
+exit
+
+npm install prosemirror-state
+npm install prosemirror-view
+npm install prosemirror-schema-basic
+npm install prosemirror-history
+npm install prosemirror-keymap
+npm install browserify
