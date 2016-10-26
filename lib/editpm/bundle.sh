56485f3fb6d821b8ab776f98dd3372061beb5b9e
Teus Benschop <teusjannette@gmail.com>
2016-10-26 13:48:24 +0200

    ProseMirror demo
    Something like the demo at https://prosemirror.net/demo/basic.html is
    now available at http://localhost:8080/editpm/index. #97

--- lib/editpm/bundle.sh
+++ lib/editpm/bundle.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-cd ~/dev/dependencies/npm
+pushd ~/dev/dependencies/npm > /dev/null
+: <<'COMMENT'
+npm install prosemirror-state
+npm install prosemirror-view
+npm install prosemirror-menu
+npm install prosemirror-schema-basic
+npm install prosemirror-schema-list
+npm install prosemirror-history
+npm install prosemirror-keymap
+npm install prosemirror-model
+npm install prosemirror-example-setup
+npm install browserify
+COMMENT
 cp ~/dev/bibledit/lib/editpm/main.js .
 ./node_modules/.bin/browserify main.js -o prosemirror.js
 cp ./node_modules/prosemirror-view/style/prosemirror.css .
+cp ./node_modules/prosemirror-menu/style/menu.css .
 rm main.js
 mv *.js ~/dev/bibledit/lib/editpm/
 mv *.css ~/dev/bibledit/lib/editpm/
-
-exit
-
-npm install prosemirror-state
-npm install prosemirror-view
-npm install prosemirror-schema-basic
-npm install prosemirror-history
-npm install prosemirror-keymap
-npm install browserify
+popd > /dev/null
