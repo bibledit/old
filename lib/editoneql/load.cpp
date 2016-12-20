8fde62c741c7a5aa73283c656acce403c754411b
Teus Benschop <teusjannette@gmail.com>
2016-12-20 17:13:21 +0100

    Cleanup #421

--- lib/editoneql/load.cpp
+++ lib/editoneql/load.cpp
@@ -67,6 +67,7 @@ string editoneql_load (void * webserver_request)
   if (!verses.empty ()) highest_verse = verses.back ();
   
   string editable_usfm = usfm_get_verse_text (chapter_usfm, verse);
+  
   string prefix_usfm = usfm_get_verse_range_text (chapter_usfm, 0, verse - 1, editable_usfm);
   string suffix_usfm = usfm_get_verse_range_text (chapter_usfm, verse + 1, highest_verse, editable_usfm);
   
