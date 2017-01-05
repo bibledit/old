1cf209e8260694546941fe9e1cb7098118c0b122
Teus Benschop <teusjannette@gmail.com>
2017-01-05 16:44:34 +0100

    Cleanup

--- lib/config.h
+++ lib/config.h
@@ -68,7 +68,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Define whether to enable unit tests */
-#define HAVE_UNITTESTS 1
+/* #undef HAVE_UNITTESTS */
 
 /* Define whether to compile on Windows */
 /* #undef HAVE_WINDOWS */
@@ -98,7 +98,7 @@
 #define STDC_HEADERS 1
 
 /* Define whether unit tests are enabled */
-#define UNITTESTS "yes"
+#define UNITTESTS "no"
 
 /* Version number of package */
 #define VERSION "1.0.670"
