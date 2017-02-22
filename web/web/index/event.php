7c2ae4d18a3b2e8489ca97e79a45f9ab16f5ddca
Teus Benschop <teusjannette@gmail.com>
2017-02-22 19:22:39 +0100

    Archive bibledit-web

--- web/web/index/event.php
+++
@@ -1,8 +0,0 @@
-<?php
-header ('Content-Type: text/event-stream');
-header ('Cache-Control: no-cache');
-
-$time = time ();
-echo "data: $time \n\n";
-
-?>
--- web/web/setup/client.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Whether to run in client mode.
-// Set it to false to have the default server mode.
-// Set it to true: Run in client mode admin.
-$client_mode = true;
-?>
